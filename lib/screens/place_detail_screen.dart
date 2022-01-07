d401fea25d6458518efec811e7e65c8ac22faab8
@eguzman2 <eguzman@buk.cl>
2022-01-06 22:32:25 -0300

    refactor

--- lib/screens/place_detail_screen.dart
+++ lib/screens/place_detail_screen.dart
@@ -5,13 +5,15 @@ import 'package:flutter_complete_guide/providers/great_places.dart';
 import 'package:provider/provider.dart';
 
 class PlaceDetailScreen extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     final id = ModalRoute.of(context).settings.arguments;
-    final selectedPlace = Provider.of<GreatPlaces>(context, listen: false).findById(id);
+    final selectedPlace =
+        Provider.of<GreatPlaces>(context, listen: false).findById(id);
     return Scaffold(
-      appBar: AppBar(title: Text('test'),),
+      appBar: AppBar(
+        title: Text('test'),
+      ),
     );
   }
 }
